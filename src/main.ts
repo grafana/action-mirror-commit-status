26f8872781816290d7201e4f562b7ef525eec3d2
Iain Lane <iain@orangesquash.org.uk>
2024-05-31 09:58:18 +0000

    Print out the payload

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import * as core from '@actions/core';
-import { wait } from './wait';
+import { context, getOctokit } from '@actions/github';
+import { GitHub } from '@actions/github/lib/utils';
+import { StatusEvent } from '@octokit/webhooks-definitions/schema';
+
+type Octokit = InstanceType<typeof GitHub>;
+
+async function handleStatusEvent(octokit: Octokit) {
+  const statusPayload = context.payload as StatusEvent;
+
+  core.info('Status event:');
+  core.info(JSON.stringify(statusPayload, null, 2));
+}
 
 /**
  * The main function for the action.
@@ -7,18 +18,19 @@ import { wait } from './wait';
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
+    const token = core.getInput('github-token', { required: true });
+    const octokit = getOctokit(token);
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`);
+    const fromCheck = core.getInput('from-check', { required: true });
+    const toCheck = core.getInput('to-check', { required: true });
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString());
+    switch (context.eventName) {
+      case 'status':
+        await handleStatusEvent(octokit);
+        break;
+      default:
+        core.setFailed(`The event ${context.eventName} is not supported.`);
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message);
